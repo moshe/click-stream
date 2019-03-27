6c7bf469dcca3b6120e7a4a89ac6d8074800ff4c
Moshe Zada <zada.moshe@gmail.com>
2019-03-27 21:52:08 +0200

    Get encoding from context

--- click_stream.py
+++ click_stream.py
@@ -15,24 +15,24 @@ except ImportError:  # Python3
 
 
 __url__ = 'https://github.com/moshe/click-stream'
-__version__ = '0.0.7'
+__version__ = '0.0.8'
 
 
 class Stream(click.ParamType):
     name = 'stream'
     SUPPORTED_SCHEMES = ('http', 'https')
 
-    def __init__(self, file_mode='r', open_kwargs=None):
+    def __init__(self, file_mode='r'):
         self.file_mode = file_mode
-        self.open_kwargs = open_kwargs
-        if open_kwargs is None:
-            self.open_kwargs = {}
 
     def convert(self, value, param, ctx):
         if value == '-':
             return sys.stdin
         if os.path.exists(value):
-            return open(value, self.file_mode, **self.open_kwargs)
+            kwargs = {}
+            if ctx.obj and ctx.obj.get('encoding'):
+                kwargs['encoding'] = ctx.obj.get('encoding')
+            return open(value, self.file_mode, **kwargs)
         url = urlparse(value)
         if url.scheme not in self.SUPPORTED_SCHEMES:
             self.fail('%s scheme is not supported' % url.scheme)
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except Exception:
 setup(
     name='click-stream',
     author='Moshe Zada',
-    version='0.0.7',
+    version='0.0.8',
     keywords=['click', 'stream', 'cli', 'url'],
     url='https://github.com/Moshe/click-stream',
     py_modules=['click_stream'],
